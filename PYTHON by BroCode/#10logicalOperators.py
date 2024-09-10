c8c0b3cc46f850df84a536f3e1a5b439ad0875b2
Dween Mohammad <dween.md01@gmail.com>
2024-09-11 01:11:31 +0600

    up

--- PYTHON by BroCode/#10logicalOperators.py
+++ PYTHON by BroCode/#10logicalOperators.py
@@ -4,4 +4,20 @@
 #                not = inverts the condition(not False, not True)
 
 
-temp =25 
+temp =30 
+is_raining=False
+is_sunny=True
+
+#example of or
+if temp>35 or temp<0 or is_raining:
+    print("the outdoor event is cancelled")
+else:
+    print (" The outdoor event is still scheduled ")
+
+#Example of and
+if temp>=28 and is_sunny:
+    print("It is hot outside🥵")
+    print("It is sunny☀️")
+elif temp<=0 and is_sunny:
+    print("It is cold outside🥶")
+    print("it is sunny☀️")
